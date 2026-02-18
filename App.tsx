7e580904b41777634d9343c6b0474540979cd4f4
enricofortin <fortinc69@gmail.com>
2026-02-18 12:01:42 +0100

    Add CookieConsent component to App

--- App.tsx
+++ App.tsx
@@ -7,6 +7,7 @@ import WhatIDo from './components/WhatIDo';
 import Services from './components/Services';
 import Values from './components/Values';
 import Footer from './components/Footer';
+import CookieConsent from './components/CookieConsent';
 
 const App: React.FC = () => {
   return (
@@ -20,6 +21,7 @@ const App: React.FC = () => {
         <Values />
       </main>
       <Footer />
+      <CookieConsent />
     </div>
   );
 };
